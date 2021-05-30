32414fea59164eea02b6ce2c4d40256b6091be0c
Christina Pagan <enterprise@christinas-mbp.home>
2021-05-30 00:06:27 -0400

    login pop-box created. user connects to profile upon login or account creation

--- server.js
+++ server.js
@@ -40,7 +40,8 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 //connection to db and server
-sequelize.sync({ force: false}).then(() => {
+sequelize.sync({ force: true}).then(() => {
   app.listen(PORT, () => console.log(`Now listening on port: ${PORT}...`))
 })
 
+ 
